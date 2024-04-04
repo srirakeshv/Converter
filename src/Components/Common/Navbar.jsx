d2799850f5aa9fed72ba5fb8b5bb8ecdda0b7087
srirakeshv <srirakeshv@gmail.com>
2024-04-04 18:41:56 +0530

    api changed

--- src/Components/Common/Navbar.jsx
+++ src/Components/Common/Navbar.jsx
@@ -1,11 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const Navbar = () => {
+function UnsplashImages() {
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        const response = await fetch(
+          `https://api.unsplash.com/photos/?client_id=oCWdYYvYZwAIRfOW7O9pBKpPKVsaPpayb8VEqYolSQU`
+        );
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch images");
+        }
+
+        const data = await response.json();
+        console.log(data);
+        console.log(data.length);
+        setImages(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching images:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchImages();
+  }, []);
+
   return (
     <div>
-      <nav></nav>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          {images.map((image) => (
+            <img
+              key={image.id}
+              src={image.urls.small}
+              alt={image.alt_description}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
-};
+}
 
-export default Navbar;
+export default UnsplashImages;
