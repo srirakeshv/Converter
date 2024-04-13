6c2ac5a1d0b2a496f1a2c9f04f063b9367dc6ae9
srirakeshv <srirakeshv@gmail.com>
2024-04-13 17:07:56 +0530

    updated title in homepage

--- src/Pages/HomePage/Homepage.jsx
+++ src/Pages/HomePage/Homepage.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import UnsplashImages from "../../Components/Home/UnsplashImages";
 import Navbar from "../../Components/Common/Navbar";
 
 const Homepage = () => {
+  useEffect(() => {
+    document.title = "Splash";
+  });
   return (
     <div className="bg-sky-950" style={{ minHeight: "100vh" }}>
       <Navbar />
